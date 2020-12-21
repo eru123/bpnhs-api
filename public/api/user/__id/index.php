8f7c263994b1f83bf1e0d5a5e194dd8ae51d3ca0
eru123 <yeoligoakino@gmail.com>
2020-12-22 07:31:53 +0800

    added account basic info api

--- public/api/user/__id/index.php
+++ public/api/user/__id/index.php
@@ -1,5 +1,24 @@
 <?php
 
-echo "<pre>";
+use \Bpnhs\Resolve;
+use \Bpnhs\User;
 
-var_dump($this->route->params);
+$users = new User($this->pdo);
+
+$res = ["error"=>"Invalid user"];
+
+$user_id = (int)$this->route->params["id"];
+
+$user = $users->row(["id"=>$user_id]);
+
+if(count($user) > 0){
+    $user["id"] = (int)$user["id"];
+
+    unset($user["pass"]);
+    unset($user["timestamp"]);
+    unset($user["phone"]);
+    unset($user["email"]);
+    unset($user["address"]);
+} else $user = $res;
+
+Resolve::json((array)$user);

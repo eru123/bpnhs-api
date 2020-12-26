0edd2e1d8b6f3346792ee2e636cbf0338c427156
eru123 <yeoligoakino@gmail.com>
2020-12-26 11:29:07 +0800

    Prevent deprecated message

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE);
 
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Methods: GET, POST');
